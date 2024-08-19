800f86e4b12805f4bac65e00e0e168bfa6bb735a
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2024-08-19 19:48:33 -0300

    traspaso de joaquin a ingresar dinero

---
+++ Aplicacion/BankArgAPP/app/src/main/java/com/lalita/bankargapp/IngresarDineroActivity.java
@@ -0,0 +1,160 @@
+package com.lalita.bankargapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.MenuItem;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class IngresarDineroActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar_dinero);
+
+        /*--- Boton en el tool bar que lleva al perfil---*/
+
+        View btnPerfil = findViewById(R.id.account_cir);
+        btnPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IngresarDineroActivity.this, PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*--- lleva al home ---*/
+        View btnHome = findViewById(R.id.rectangle_2);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IngresarDineroActivity.this, ProductActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Referencia al Spinner
+        Spinner miSpinner = findViewById(R.id.miSpinner);
+
+        // Opciones del Spinner
+        String[] opciones = {"Pago Fácil", "Rappi Pago"};
+
+        // Adaptador para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Asigna el adaptador al Spinner
+        miSpinner.setAdapter(adapter);
+
+        // Maneja la selección de una opción
+        miSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Obtén la opción seleccionada
+                String seleccion = (String) parent.getItemAtPosition(position);
+
+                // Muestra un mensaje con la opción seleccionada
+                Toast.makeText(IngresarDineroActivity.this, "Seleccionaste: " + seleccion, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        /*---------------------Hooks------------------------*/
+        drawerLayout=findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                IngresarDineroActivity activity = IngresarDineroActivity.this;
+                if (itemId == R.id.nav_product) {
+                    Intent intent = new Intent(activity, ProductActivity.class);
+                    Log.i("MENU_DRAWER_TAG", "Home is selected");
+                    startActivities(new Intent[]{intent});
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                } else if (itemId == R.id.nav_banking) {
+                    Intent intent = new Intent(activity, BankingActivity.class);
+                    Log.i("MENU_DRAWER_TAG", "Banking is selected");
+                    startActivities(new Intent[]{intent});
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                else if (itemId == R.id.nav_contact) {
+                    Intent intent = new Intent(activity, ContactActivity.class);
+                    Log.i("MENU_DRAWER_TAG", "Contact is selected");
+                    startActivities(new Intent[]{intent});
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (itemId == R.id.nav_support) {
+                    Intent intent = new Intent(activity, SupportActivity.class);
+                    Log.i("MENU_DRAWER_TAG", "Support is selected");
+                    startActivities(new Intent[]{intent});
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (itemId == R.id.nav_logout) {
+                    Toast.makeText(activity, "Singing Out", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(activity, MainActivity.class);
+                    Log.i("MENU_DRAWER_TAG", "Logout is selected");
+                    startActivities(new Intent[]{intent});
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
